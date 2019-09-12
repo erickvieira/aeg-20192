ca45e3073e4dd18d5f77b011fbb8426ab2f0048b
erickvieira <erick-vieira-s@outlook.com>
2019-09-12 20:19:16 -0300

    correcao na identacao

--- Grafo_Interface.py
+++ Grafo_Interface.py
@@ -1,10 +1,11 @@
 import abc
 
-class Grafo(metaclass=abc.ABCMeta):
-# Classe base abstrata para representações de grafos
 
-    def __init__(self, direct = True):
-	    # Grafo se direct=False ou Digrafo se direct=True.
+class Grafo(metaclass=abc.ABCMeta):
+    # Classe base abstrata para representações de grafos
+
+    def __init__(self, direct=True):
+            # Grafo se direct=False ou Digrafo se direct=True.
         self.n, self.m, self.orientado = None, None, orientado
 
     @abc.abstractmethod
@@ -14,35 +15,34 @@ class Grafo(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def V(self):
-	    # Retorna a lista de vértices.
-	    pass
+            # Retorna a lista de vértices.
+        pass
 
     @abc.abstractmethod
     def E(self):
-	    # Retorna lista de arestas uv.
-	    pass
+        # Retorna lista de arestas uv.
+        pass
 
     @abc.abstractmethod
     def RemoverAresta(self, u, v):
-	    # Remove a aresta uv.		
+        # Remove a aresta uv.
         pass
 
     @abc.abstractmethod
     def AdicionarAresta(self, u, v):
-	    # Adiciona aresta uv.
-	    pass
+        # Adiciona aresta uv.
+        pass
 
     @abc.abstractmethod
     def SaoViz(self, u, v):
-	    # Retorna True se e somente se uv é uma aresta.
-	    pass
+        # Retorna True se e somente se uv é uma aresta.
+        pass
 
     @abc.abstractmethod
-    def Viz(self, v, Tipo = "*", Fechada=False):
-	    """
-	    Retorna lista de vértices vizinhos do vértice v. Se Fechada=True, o próprio v é incluido na lista.
-	    Tipo="*" significa listar todas as arestas incidentes em v. Se G é orientado, 
-	    Tipo="+" (resp. "-") significa listar apenas as arestas de saída (resp. entrada) de v.
-	    """	
-	    pass
-
+    def Viz(self, v, Tipo="*", Fechada=False):
+        """
+        Retorna lista de vértices vizinhos do vértice v. Se Fechada=True, o próprio v é incluido na lista.
+        Tipo="*" significa listar todas as arestas incidentes em v. Se G é orientado, 
+        Tipo="+" (resp. "-") significa listar apenas as arestas de saída (resp. entrada) de v.
+        """
+        pass
