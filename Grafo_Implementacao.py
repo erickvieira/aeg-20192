9273bf8cf867be956ee9c8f5b732d6ed283c1f6c
erickvieira <erick-vieira-s@outlook.com>
2019-09-17 22:37:00 -0300

    implementacao das funcoes inicias do grafo

---
+++ Grafo_Implementacao.py
@@ -0,0 +1,65 @@
+from Grafo_Interface import IGrafo
+from random import randint
+from string import ascii_uppercase
+
+class Grafo(IGrafo):
+
+    def __init__(self, direct = True):
+        self.nos = {}
+
+    def ChecarListaVazia(self):
+        vazio = True
+        for no in self.nos:
+            vazio = vazio and no == []
+        return vazio
+
+    def DefinirN(self, n = 1):
+        self.nos = {}
+        for i in range(0, n):
+            self.nos[ascii_uppercase[i]] = []
+        return self.nos
+
+    def V(self):
+        vertices = []
+        for no in self.nos:
+            vertices.append(no)
+        return vertices
+
+    def E(self):
+        arestas = []
+        if self.ChecarListaVazia():
+            return arestas
+        for no in self.nos:
+            for viz in self.nos[no]:
+                if (viz, no) not in arestas:
+                    arestas.append((no, viz))
+        return arestas
+
+    def RemoverAresta(self, u = '', v = ''):
+        if v == '' or u == '':
+            return None
+        if self.ChecarListaVazia():
+            raise Exception('ERRO: grafo vazio. Por favor, use o medoto ".DefinirN()" para inicializar a estrutura.')
+        if (u not in self.nos) or (v not in self.nos):
+            raise Exception('ERRO: a aresta (%s, %s) nao existe no grafo atual.' % (u, v))
+        else:
+            self.nos[u].remove(v)
+            self.nos[v].remove(u)
+        return [(u, v), (v, u)]
+
+    def AdicionarAresta(self, u = '', v = ''):
+        if v in self.nos[u]:
+            return self.nos
+        if v == '' or u == '':
+            return self.nos
+        if self.ChecarListaVazia():
+            raise Exception('ERRO: grafo vazio. Por favor, use o medoto ".DefinirN()" para inicializar a estrutura.')
+        self.nos[u].append(v)
+        self.nos[v].append(u)
+        return self.nos
+
+    def SaoViz(self, u = '', v = ''):
+        return u in self.nos[v] and v in self.nos[u]
+
+    def Viz(self, v, Tipo="*", Fechada=False):
+        return self.nos[v]
--- Grafo_Interface.py
+++ Grafo_Interface.py
@@ -1,44 +1,45 @@
-import abc
+from abc import ABCMeta, abstractmethod
 
 
-class Grafo(metaclass=abc.ABCMeta):
+class IGrafo(metaclass=ABCMeta):
     # Classe base abstrata para representações de grafos
 
-    def __init__(self, direct=True):
+    @abstractmethod
+    def __init__(self, orientado=False):
             # Grafo se direct=False ou Digrafo se direct=True.
-        self.n, self.m, self.orientado = None, None, orientado
+        self.n, self.m, self.orientado = orientado
 
-    @abc.abstractmethod
+    @abstractmethod
     def DefinirN(self, n):
         # Define o número n de vértices.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def V(self):
             # Retorna a lista de vértices.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def E(self):
         # Retorna lista de arestas uv.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def RemoverAresta(self, u, v):
         # Remove a aresta uv.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def AdicionarAresta(self, u, v):
         # Adiciona aresta uv.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def SaoViz(self, u, v):
         # Retorna True se e somente se uv é uma aresta.
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def Viz(self, v, Tipo="*", Fechada=False):
         """
         Retorna lista de vértices vizinhos do vértice v. Se Fechada=True, o próprio v é incluido na lista.
