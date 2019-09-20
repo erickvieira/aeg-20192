670e725a44ecb8b23d961aa1ccf46e9115aac159
Erick Vieira <erick-vieira-s@outlook.com>
2019-09-19 22:31:48 -0300

    melhorias nos scripts de exeplificacao e apoio

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
 from Grafo_Implementacao import Grafo
+from Matriz_Adjacencias import Matriz_Adjacencias
 from random import randint
 import Utils as utils
 
 grafo = Grafo()
-limite = randint(1, 20)
-print('> GERANDO GRAFO DE %02d VERTICES' % limite)
+limite = randint(1, 26)
+print('> GERANDO GRAFO DE %02d VERTICES\n\n' % limite)
 grafo.DefinirN(limite)
 
 print('= ADICIONANDO ARESTAS =================')
 grafo = utils.gerarGrafo(limite, grafo)
-print(grafo.nos)
+print(grafo)
 print('=======================================\n')
 
 print('= CONJUNTOS ===========================')
 print('- DE VERTICES: %s' % grafo.V())
 print('- DE ARESTAS:  %s' % grafo.E())
+print('=======================================\n')
+
+matriz = Matriz_Adjacencias(grafo)
+matriz.CalcularMatriz()
+
+print('= MATRIZ DE ADJACENCIAS ===============')
+print(matriz)
 print('=======================================\n')
