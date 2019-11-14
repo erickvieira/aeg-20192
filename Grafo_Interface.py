76b9281771bfb494de04c0a27374dab72b315595
erickvieira <erick-vieira-s@outlook.com>
2019-11-14 18:27:50 -0300

    implementação da busca irrestrita em profundidade e da busca de ciclos

--- Grafo_Interface.py
+++ Grafo_Interface.py
@@ -50,7 +50,7 @@ class IGrafo(metaclass=ABCMeta):
     def __vizinhos_nao_marcados_de_v(self, marcados = {}, v = ""):
         return filter(lambda x: not marcados[x], self.Viz(v))
     
-    def BuscaLargura(self, callback = lambda u, v: print((u, v))):
+    def BuscaLargura(self, visitarNo = lambda u, v: print((u, v))):
         vertices = self.V()
         marcados = { key: False for key in vertices }
         marcados[vertices[0]] = True
@@ -59,15 +59,15 @@ class IGrafo(metaclass=ABCMeta):
         while F:
             for w in filter(lambda x: not marcados[x], self.Viz(F[0])):
                 if not marcados[w]:
-                    callback(F[0], w)
+                    visitarNo(F[0], w)
                     marcados[w] = True
                     F.append(w)
                 else:
                     if w in F:
-                        callback(F[0], w)
+                        visitarNo(F[0], w)
             marcados[F.pop(0)] = True
     
-    def BuscaProfundidade(self, callback = lambda u, v: print((u, v))):
+    def BuscaProfundidade(self, visitarNo = lambda u, v: print((u, v))):
         vertices = self.V()
         marcados = { key: False for key in vertices }
         P = []
@@ -76,15 +76,15 @@ class IGrafo(metaclass=ABCMeta):
             P.insert(0, v)
             for w in filter(lambda x: not marcados[x], self.Viz(v)):
                 if not marcados[w]:
-                    callback(P[0], w)
+                    visitarNo(P[0], w)
                     p(w)
                 else:
                     if w in P:
-                        callback(P[0], w)
-            marcados[P.pop(0)]
+                        visitarNo(P[0], w)
+            marcados[P.pop(0)] = True
         p(vertices[0])
     
-    def BuscaCompleta(self, callback = lambda u, v: print((u, v))):
+    def BuscaCompleta(self, visitarNo = lambda u, v: print((u, v))):
         if not self._orientado:
             raise Exception('FATAL: impossível rodar uma BuscaCompleta em um grafo que não seja DÍGRAFO.')
         else :
@@ -95,16 +95,54 @@ class IGrafo(metaclass=ABCMeta):
                 marcados[v] = True
                 P.insert(0, v)
                 for w in filter(lambda x: not marcados[x], self.Viz(v)):
-                    callback(P[0], w)
+                    visitarNo(P[0], w)
                     if not marcados[w]:
                         p(w)
-                marcados[P.pop(0)]
+                marcados[P.pop(0)] = True
             for r in vertices:
                 if not marcados[r]:
                     p(r)
     
-    def ComponentesFortementeConexas(self, callback = lambda u, v: print((u, v))):
+    def ComponentesFortementeConexas(self, visitarNo = lambda u, v: print((u, v))):
         if not self._orientado:
             raise Exception('FATAL: impossível rodar procurar ComponentesFortementeConexas em um grafo que não seja DÍGRAFO.')
         else:
             pass
+    
+    def BuscaIrProf(self, visitarNo = lambda u, v: print((u, v))):
+        vertices = self.V()
+        marcados = { key: False for key in vertices }
+        P = []
+        def p(v):
+            marcados[v] = True
+            P.insert(0, v)
+            for w in filter(lambda x: not marcados[x], self.Viz(v)):
+                if w not in P:
+                    visitarNo(v, w)
+                    p(w)
+            marcados[P.pop(0)] = False
+        p(vertices[0])
+    
+    def BuscarCiclosSimples(
+        self, 
+        visitarNo = lambda u, v: print((u, v)),
+        capturarCiclos = lambda ciclos: print(ciclos)
+    ):
+        vertices = self.V()
+        marcados = { key: False for key in vertices }
+        ciclos = { key: [] for key in vertices }
+        P = []
+        def p(v):
+            marcados[v] = True
+            P.insert(0, v)
+            for w in self.Viz(v):
+                if w not in P:
+                    visitarNo(v, w)
+                    p(w)
+                if w not in ciclos[v]:
+                    ciclos[v].append(w)
+            marcados[P.pop(0)] = False
+        p(vertices[0])
+        for ciclo in ciclos:
+            if len(ciclos[ciclo]) > 2:
+                capturarCiclos(ciclos[ciclo])
--- __main__.py
+++ __main__.py
@@ -1,15 +1,16 @@
 #!/bin/python3
 from Grafo_Lista_Adjacencia import Grafo
 
-print('Busca completa - Grafo 4 posições')
+print('Busca irrestrita - Grafo 4 posições')
 
-grafo = Grafo(True)
+grafo = Grafo()
 grafo.DefinirN(4)
 grafo.AdicionarAresta('A', 'B')
 grafo.AdicionarAresta('A', 'D')
 grafo.AdicionarAresta('C', 'B')
+grafo.AdicionarAresta('C', 'A')
 
 print('Arestas: {}'.format(grafo.E()))
 
 print('\nRodando a busca')
-grafo.BuscaCompleta()
+grafo.BuscarCiclosSimples()
